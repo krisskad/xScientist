00c91d4b1e93d56358368e950daf9872554a5475
krishna_kadam <krishna.kadam@ongil.io>
2022-07-01 14:56:39 +0530

    db update and cors error fix

--- MAIN/production_settings.py
+++ MAIN/production_settings.py
@@ -32,7 +32,8 @@ DEBUG = False
 ALLOWED_HOSTS = ['localhost', 'xscientist.in', 'xscientist.herokuapp.com', '0.0.0.0', '127.0.0.1', 'www.xscientist.in']
 
 # Application definition
-
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
--- homepage/urls.py
+++ homepage/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    re_path(r'^$', views.index, name='index'),
     path('send_email/', views.send_email, name="index"),
     path('send_email/home/', views.index, name="index")
 ]
